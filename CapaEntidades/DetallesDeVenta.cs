09b5144656feac2497b8fa20cc3b33b5c8b76ff1
segur <segur@JEREMYGAMER8450>
2024-04-11 14:26:53 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidades/DetallesDeVenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class DetallesDeVenta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DetalleVentaId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public int Precio { get; set; }
+
+        [Required]  // Para el ProductoId todo esto
+        public int ProductoId { get; set; }
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; } //.
+
+
+        [Required]  // Para VentaId todo esto
+        public int VentaId { get; set; }
+        [ForeignKey("VentaId")]
+        public Venta Venta { get; set; } //.
+
+    }
+}
+
---
+++ CapaEntidades/Venta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Venta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VentaId { get; set; }
+        [Required]
+        public DateTime FechaVenta { get; set; }
+        [Required]
+        public decimal Total { get; set; }
+
+        // propiedades para navegacion
+        public List<DetallesDeVenta> Detalles { get; set; }
+    }
+}
